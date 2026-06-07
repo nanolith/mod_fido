3e6b080a50864ff7144fd120b1a0427edf14e609
Justin Handville <nanolith@gmail.com>
2026-06-06 23:46:52 -0400

    auth_cache_entry_cmp decl.

--- src/auth_cache.h
+++ src/auth_cache.h
@@ -24,3 +24,17 @@ struct auth_cache_entry
 
     RB_ENTRY(auth_cache_entry) link;
 };
+
+/**
+ * \brief Compare two auth_cache entries.
+ *
+ * \param lhs           The left-hand side of the comparison.
+ * \param rhs           The right-hand side of the comparison.
+ *
+ * \returns An integer value describing the comparison result:
+ *      - a negative value if lhs < rhs.
+ *      - a positive value if lhs > rhs.
+ *      - zero if lhs == rhs.
+ */
+int
+auth_cache_entry_cmp(auth_cache_entry *lhs, auth_cache_entry *rhs);
