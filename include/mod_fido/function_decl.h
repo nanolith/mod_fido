bdb843d932599a9be6aaf3b26abe08dc1e17909e
Justin Handville <nanolith@gmail.com>
2026-06-08 00:40:14 -0400

    FN_DECL_MUST_CHECK macro.

---
+++ include/mod_fido/function_decl.h
@@ -0,0 +1,27 @@
+/**
+ * \file mod_fido/function_decl.h
+ *
+ * \brief Function declaration macros for mod_fido.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+/*
+ * With GCC 3.4 and clang, we can force a warning / error if the return value
+ * of a function isn't checked.
+ */
+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && !defined(FN_DECL_MUST_CHECK)
+# if (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || (__GNUC__ > 3)
+#  define FN_DECL_MUST_CHECK __attribute__((warn_unused_result))
+# endif
+#endif
+
+/*
+ * For other compilers, skip this check.
+ */
+#ifndef FN_DECL_MUST_CHECK
+# define FN_DECL_MUST_CHECK
+#endif
