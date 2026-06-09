5e81877068e208afc46248a685d3ddabd43080ca
Justin Handville <nanolith@gmail.com>
2026-06-09 00:47:08 -0400

    mod_fido_instance_hooked_sys_ioctl impl.

---
+++ src/mod_fido_instance_hooked_sys_ioctl.c
@@ -0,0 +1,58 @@
+/**
+ * \file mod_fido_instance_hooked_sys_ioctl.c
+ *
+ * \brief Hook for the ioctl syscall.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+/**
+ * \brief IOCTL hook for mod_fido.
+ *
+ * \param td            The thread on which this system call was made.
+ * \param args          The arguments for this system call.
+ *
+ * \returns a error code. 0 on success and an error number on failure.
+ */
+int
+mod_fido_instance_hooked_sys_ioctl(struct thread *td, void *args)
+{
+    int retval;
+    struct ioctl_args* uap = (struct ioctl_args*)args;
+
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        mod_fido_instance_hooked_sys_ioctl, td, args);
+
+    switch (uap->com)
+    {
+        case TIOCSETVERAUTH:
+            retval =
+                mod_fido_instance_ioctl_TIOCSETVERAUTH_handler(
+                    mod_fido_global_inst, td, uap);
+            break;
+
+        case TIOCCHKVERAUTH:
+            retval =
+                mod_fido_instance_ioctl_TIOCCHKVERAUTH_handler(
+                    mod_fido_global_inst, td, uap);
+            break;
+
+        case TIOCCLRVERAUTH:
+            retval =
+                mod_fido_instance_ioctl_TIOCCLRVERAUTH_handler(
+                    mod_fido_global_inst, td, uap);
+            break;
+
+        default:
+            retval = mod_fido_global_inst->old_sys_ioctl(td, args);
+            break;
+    }
+
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        mod_fido_instance_hooked_sys_ioctl, retval, td, args);
+
+    return retval;
+}
