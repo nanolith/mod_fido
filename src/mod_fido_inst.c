75da0e72f650cc158315583c743d6323b2d292b9
Justin Handville <nanolith@gmail.com>
2026-06-09 01:41:12 -0400

    mod_fido_global_inst impl.

---
+++ src/mod_fido_inst.c
@@ -0,0 +1,12 @@
+/**
+ * \file mod_fido_inst.c
+ *
+ * \brief The global instance variable.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+mod_fido_instance* mod_fido_global_inst = NULL;
