24491e551f781d1a962235da66346f8c59e54780
Justin Handville <nanolith@gmail.com>
2026-06-11 18:24:49 -0400

    kernel_mtx_valid decl.

---
+++ include/mod_fido/properties/kernel.h
@@ -0,0 +1,14 @@
+/**
+ * \file mod_fido/properties/kernel.h
+ *
+ * \brief Model checking properties for kernel objects.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+
+extern int kernel_mtx_valid;
