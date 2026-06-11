6c8226e7824ac517193dcb70089fca79955d6229
Justin Handville <nanolith@gmail.com>
2026-06-11 18:37:13 -0400

    auth_cache_table_next_simple_loop impl.

---
+++ models/shadow/auth_cache/auth_cache_table_next_simple_loop.c
@@ -0,0 +1,35 @@
+/**
+ * \file models/shadow/auth_cache/auth_cache_table_next_simple_loop.c
+ *
+ * \brief Get the next value for the simple loop shadow.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+extern int auth_cache_table_count;
+
+auth_cache_entry* auth_cache_table_next(auth_cache_entry* root)
+{
+    auth_cache_entry* entry;
+
+    if (auth_cache_table_count > 0)
+    {
+        --auth_cache_table_count;
+
+        int retval = auth_cache_entry_create_random(&entry);
+        if (0 != retval)
+        {
+            /* trim this branch. */
+            MODEL_ASSUME(0);
+        }
+
+        return entry;
+    }
+    else
+    {
+        return NULL;
+    }
+}
