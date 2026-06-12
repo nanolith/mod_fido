bd1bb0557c82803cd1d92d742a3bcd5907b4785f
Justin Handville <nanolith@gmail.com>
2026-06-11 21:03:12 -0400

    property_mod_fido_instance_valid shadow impl.

---
+++ models/shadow/mod_fido/property_mod_fido_instance_valid.c
@@ -0,0 +1,27 @@
+/**
+ * \file models/shadow/mod_fido/property_mod_fido_instance_valid.c
+ *
+ * \brief Verify that a \ref mod_fido_instance is valid.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+/**
+ * \brief Valid \ref mod_fido_instance.
+ *
+ * \param inst          The instance to check.
+ *
+ * \returns 1 if the instance is valid and 0 otherwise.
+ */
+int
+property_mod_fido_instance_valid(
+    const mod_fido_instance* inst)
+{
+    MODEL_CHECK_OBJECT_READ(inst, sizeof(*inst));
+    MODEL_ASSERT(property_mtx_valid(&inst->fido_mtx));
+
+    return 1;
+}
