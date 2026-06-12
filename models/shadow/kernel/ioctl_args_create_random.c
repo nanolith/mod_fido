b330f909e7eb8fb2ad1d2cb28b7522a48558cd89
Justin Handville <nanolith@gmail.com>
2026-06-12 03:47:17 -0400

    ioctl_args_create_random shadow impl.

---
+++ models/shadow/kernel/ioctl_args_create_random.c
@@ -0,0 +1,31 @@
+/**
+ * \file models/shadow/kernel/ioctl_args_create_random.c
+ *
+ * \brief Helpers to create and release ioctl_args instances.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+int nondet_int();
+
+int ioctl_args_create_random(struct ioctl_args** args)
+{
+    *args =
+        (struct ioct_args*)malloc(sizeof(**args), M_FIDO, M_WAITOK | M_ZERO);
+    if (NULL == *args)
+    {
+        return 1;
+    }
+
+    (*args)->fd = nondet_int();
+
+    return 0;
+}
+
+void ioctl_args_release(struct ioctl_args* args)
+{
+    free(args);
+}
