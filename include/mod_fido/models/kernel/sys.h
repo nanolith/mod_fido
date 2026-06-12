10edd99e688c2749343b8d207d0aa847ae6542df
Justin Handville <nanolith@gmail.com>
2026-06-11 20:34:36 -0400

    MALLOC_DECLARE decl.

---
+++ include/mod_fido/models/kernel/sys.h
@@ -0,0 +1,14 @@
+/**
+ * \file models/kernel/sys.h
+ *
+ * \brief Simplified system declarations for model checking.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <pthread.h>
+
+#define MALLOC_DECLARE(x) extern const int x
