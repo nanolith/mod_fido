0b28dac620215e4852bc11f8e0e5a04d8f8c56e3
Justin Handville <nanolith@gmail.com>
2026-06-11 23:30:08 -0400

    mod_fido_instance_hooked_sys_ioctl shadow impl.

---
+++ models/shadow/mod_fido/mod_fido_instance_hooked_sys_ioctl.c
@@ -0,0 +1,26 @@
+/**
+ * \file models/shadow/mod_fido/mod_fido_instance_hooked_sys_ioctl.c
+ *
+ * \brief Shadow implementation of \ref mod_fido_instance_hooked_sys_ioctl.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+static int nondet_retval();
+
+int
+mod_fido_instance_hooked_sys_ioctl(struct thread *td, void *args)
+{
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        mod_fido_instance_hooked_sys_ioctl, td, args);
+
+    int retval = nondet_retval();
+
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        mod_fido_instance_hooked_sys_ioctl, retval, td, args);
+
+    return retval;
+}
