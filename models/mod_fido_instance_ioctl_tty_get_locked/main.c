24d200dc92c2f17cba83d7def1e56caada332806
Justin Handville <nanolith@gmail.com>
2026-06-13 15:37:40 -0400

    mod_fido_instance_ioctl_tty_get_locked model checks.

---
+++ models/mod_fido_instance_ioctl_tty_get_locked/main.c
@@ -0,0 +1,62 @@
+/**
+ * \file models/mod_fido_instance_ioctl_tty_get_locked/main.c
+ *
+ * \brief Model checks for \ref mod_fido_instance_ioctl_tty_get_locked.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+int main(int argc, char* argv[])
+{
+    int retval, release_retval;
+    mod_fido_instance* inst;
+    struct thread* td;
+    struct ioctl_args* args;
+
+    /* create an instance. */
+    retval = mod_fido_instance_create_random(&inst);
+    if (0 != retval)
+    {
+        MODEL_ASSUME(0);
+    }
+
+    /* create a thread. */
+    retval = thread_create_random(&td);
+    if (0 != retval)
+    {
+        MODEL_ASSUME(0);
+    }
+
+    /* create ioctl args. */
+    retval = ioctl_args_create_random(&args);
+    if (0 != retval)
+    {
+        MODEL_ASSUME(0);
+    }
+
+    /* lock the instance. */
+    mtx_lock(&inst->fido_mtx);
+
+    /* get the tty. */
+    struct tty* tp = mod_fido_instance_ioctl_tty_get_locked(inst, td, args);
+
+    /* unlock the instance. */
+    mtx_unlock(&inst->fido_mtx);
+
+    goto done;
+
+done:
+    release_retval = mod_fido_instance_release(inst);
+    if (0 != release_retval)
+    {
+        retval = release_retval;
+    }
+
+    ioctl_args_release(args);
+    thread_release(td);
+
+    return retval;
+}
