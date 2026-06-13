43dba721a4835a4c09a00e781b018466ade82f33
Justin Handville <nanolith@gmail.com>
2026-06-13 15:33:30 -0400

    vnode_create_random decl.

--- include/mod_fido/models/kernel/sys.h
+++ include/mod_fido/models/kernel/sys.h
@@ -127,6 +127,7 @@ cap_rights_t* cap_rights_init(cap_rights_t *rights, int capability);
 #define DTYPE_VNODE 1
 #define DTYPE_PTS 2
 
-struct tty* tty_create_random();
 struct cdev* cdev_create_random();
+struct tty* tty_create_random();
+struct vnode* vnode_create_random();
 int fileops_default_close(struct file* fp, struct thread* td);
