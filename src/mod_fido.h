9caabd82ed5cd57235bb04a1a4accf3e9d3bcff0
Justin Handville <nanolith@gmail.com>
2026-06-13 16:26:49 -0400

    mod_fido_instance_ioctl_tty_get function contract preconditions.

--- src/mod_fido.h
+++ src/mod_fido.h
@@ -471,6 +471,20 @@ struct tty*
 mod_fido_instance_ioctl_tty_get(
     mod_fido_instance* inst, struct thread *td, struct ioctl_args* args);
 
+/* function contract preconditions. */
+MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    mod_fido_instance_ioctl_tty_get, mod_fido_instance* inst, struct thread* td,
+    struct ioctl_args* args)
+        /* inst is valid. */
+        MODEL_ASSERT(property_mod_fido_instance_valid(inst));
+        /* inst is NOT locked. */
+        MODEL_ASSERT(!property_mod_fido_instance_locked(inst));
+        /* td is valid. */
+        MODEL_ASSERT(property_kernel_thread_valid(td));
+        /* args is valid. */
+        MODEL_ASSERT(property_ioctl_args_valid(args));
+MODEL_CONTRACT_PRECONDITIONS_END(mod_fido_instance_ioctl_tty_get)
+
 /**
  * \brief Hook the file options table for a given file handle.
  *
