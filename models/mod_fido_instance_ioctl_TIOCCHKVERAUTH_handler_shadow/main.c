1d3bbf4c84308ae7c3bbf307b98a4779cc78f754
Justin Handville <nanolith@gmail.com>
2026-06-14 00:40:55 -0400

    mod_fido_instance_ioctl_TIOCCHKVERAUTH_handler_shadow model checks.

---
+++ models/mod_fido_instance_ioctl_TIOCCHKVERAUTH_handler_shadow/main.c
@@ -0,0 +1,63 @@
+/**
+ * \file models/mod_fido_instance_ioctl_TIOCCHKVERAUTH_handler_shadow/main.c
+ *
+ * \brief Model checks for the shadow implementation of
+ * \ref mod_fido_instance_ioctl_TIOCCHKVERAUTH_handler.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+int nondet_int();
+
+int main(int argc, char* argv[])
+{
+    int retval, release_retval;
+    mod_fido_instance* inst;
+    struct thread* td;
+    struct ioctl_args* args;
+
+    /* create an instance. */
+    retval = mod_fido_instance_create_random(&inst);
+    if (0 != retval)
+    {
+        MODEL_ASSUME(0);
+    }
+
+    /* create a thread. */
+    retval = thread_create_random(&td);
+    if (0 != retval)
+    {
+        MODEL_ASSUME(0);
+    }
+
+    /* create ioctl args. */
+    retval = ioctl_args_create_random(&args);
+    if (0 != retval)
+    {
+        MODEL_ASSUME(0);
+    }
+
+    /* Handle a TIOCCHKVERAUTH ioctl. */
+    retval = mod_fido_instance_ioctl_TIOCCHKVERAUTH_handler(inst, td, args);
+    if (0 != retval)
+    {
+        goto done;
+    }
+
+    goto done;
+
+done:
+    release_retval = mod_fido_instance_release(inst);
+    if (0 != release_retval)
+    {
+        retval = release_retval;
+    }
+
+    ioctl_args_release(args);
+    thread_release(td);
+
+    return retval;
+}
