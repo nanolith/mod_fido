256b6edfe1eb77c809f4644a11592968ca872890
Justin Handville <nanolith@gmail.com>
2026-06-13 20:59:51 -0400

    property_error_code_is_expected_for_ioctl_syscall shadow impl.

---
+++ models/shadow/kernel/property_error_code_is_expected_for_ioctl_syscall.c
@@ -0,0 +1,20 @@
+/**
+ * \file models/shadow/kernel/property_error_code_is_expected_for_ioctl_syscall.c
+ *
+ * \brief Verify that the syscall returns an expected error code.
+ *
+ * \copyright 2026 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "mod_fido.h"
+
+bool property_error_code_is_expected_for_ioctl_syscall(int error)
+{
+    return
+        (EBADF == error
+      || ENOTTY == error
+      || EINVAL == error
+      || EFAULT == error
+      || EPERM == error);
+}
